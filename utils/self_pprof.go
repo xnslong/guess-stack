415d9e664a96cc29a1bb1bbdc304d08e543906c0
xnslong <szlon@qq.com>
2022-03-27 15:06:30 +0800

    remove pool & improve self_pprof

--- utils/self_pprof.go
+++ utils/self_pprof.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -9,27 +10,25 @@ import (
 )
 
 func DoWithPProf(outfile string, job func()) {
-	file, _ := os.OpenFile(outfile, os.O_CREATE|os.O_WRONLY, 0644)
-	defer func() {
-		file.Close()
-		log.Printf("self pprof is sampled to %s", outfile)
-	}()
-	pprof.StartCPUProfile(file)
-	defer pprof.StopCPUProfile()
+	_ = WriteToFile(outfile, func(writer io.Writer) error {
+		_ = pprof.StartCPUProfile(writer)
+		defer pprof.StopCPUProfile()
 
-	done := make(chan struct{})
-	go func() {
-		defer close(done)
-		job()
-	}()
+		done := make(chan struct{})
+		go func() {
+			defer close(done)
+			job()
+		}()
 
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGHUP, syscall.SIGTERM)
-	select {
-	case sig := <-signals:
-		log.Printf("interrupted by %s", sig)
-	case <-done:
-	}
+		signals := make(chan os.Signal, 1)
+		signal.Notify(signals, syscall.SIGINT, syscall.SIGHUP, syscall.SIGTERM)
+		select {
+		case sig := <-signals:
+			log.Printf("interrupted by %s", sig)
+		case <-done:
+		}
 
-	return
+		log.Printf("salf-pprof is written to: %s", outfile)
+		return nil
+	})
 }
